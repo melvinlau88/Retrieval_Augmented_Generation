53087517acd72372ea434f5902bc3d2600e1b0bf
Melvin <melvinlau88@outlook.com>
2026-07-06 12:08:07 +1000

    Imported more libraries, upadted old versions and loaded a webpage

--- main.py
+++ main.py
@@ -10,9 +10,21 @@ load_dotenv()
 
 import bs4
 
-from langchain import hub
+from langchain_classic import hub
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 import os
 os.environ['LANGCHAIN_TRACING_V2'] = 'true'
 os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+# Load the Webpage https://en.wikipedia.org/wiki/Python_(programming_language)
+
+loader = WebBaseLoader("https://en.wikipedia.org/wiki/Python_(programming_language)")
+docs = loader.load()
+
+print(docs[0].page_content[:500])
